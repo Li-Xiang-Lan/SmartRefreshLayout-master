667c083508a27ba47bc46a766c50006677ad1732
unknown <519097965@qq.com>
2018-05-09 16:19:46 +0800

    first commit

---
+++ refresh-layout/src/main/java/com/scwang/smartrefresh/layout/impl/ScrollBoundaryDeciderAdapter.java
@@ -0,0 +1,59 @@
+package com.scwang.smartrefresh.layout.impl;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.scwang.smartrefresh.layout.api.ScrollBoundaryDecider;
+import com.scwang.smartrefresh.layout.util.ScrollBoundaryUtil;
+
+/**
+ * 滚动边界
+ * Created by SCWANG on 2017/7/8.
+ */
+
+@SuppressWarnings("WeakerAccess")
+public class ScrollBoundaryDeciderAdapter implements ScrollBoundaryDecider {
+
+    //<editor-fold desc="Internal">
+    protected MotionEvent mActionEvent;
+    protected ScrollBoundaryDecider boundary;
+    protected boolean mEnableLoadmoreWhenContentNotFull;
+
+    void setScrollBoundaryDecider(ScrollBoundaryDecider boundary){
+        this.boundary = boundary;
+    }
+
+    void setActionEvent(MotionEvent event) {
+        //event 在没有必要时候会被设置为 null
+        mActionEvent = event;
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="ScrollBoundaryDecider">
+    @Override
+    public boolean canRefresh(View content) {
+        if (boundary != null) {
+            return boundary.canRefresh(content);
+        }
+        //mActionEvent == null 时 canRefresh 不会动态递归搜索
+        return ScrollBoundaryUtil.canRefresh(content, mActionEvent);
+    }
+
+    @Override
+    public boolean canLoadmore(View content) {
+        if (boundary != null) {
+            return boundary.canLoadmore(content);
+        }
+        if (mEnableLoadmoreWhenContentNotFull) {
+            //mActionEvent == null 时 canScrollDown 不会动态递归搜索
+            return !ScrollBoundaryUtil.canScrollDown(content, mActionEvent);
+        }
+        //mActionEvent == null 时 canLoadmore 不会动态递归搜索
+        return ScrollBoundaryUtil.canLoadmore(content, mActionEvent);
+    }
+
+    public void setEnableLoadmoreWhenContentNotFull(boolean enable) {
+        mEnableLoadmoreWhenContentNotFull = enable;
+    }
+    //</editor-fold>
+}
